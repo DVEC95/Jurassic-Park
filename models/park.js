40a93f92f9291edc1a2191696076ccb07c15c3d0
David Eastcroft <davideastcroft95@gmail.com>
2020-06-13 13:03:27 +0100

    Adds method for returning most popular dinosaur.

--- models/park.js
+++ models/park.js
@@ -4,13 +4,20 @@ const Park = function(name, ticketPrice){
   this.dinosaurs = [];
 };
 
-Park.prototype.addDinosaur = function (dinosaur) {
+Park.prototype.addDinosaur = function(dinosaur){
   this.dinosaurs.push(dinosaur);
 };
 
-Park.prototype.removeDinosaur = function (dinosaur) {
+Park.prototype.removeDinosaur = function(dinosaur){
   dinosaurIndex = this.dinosaurs.indexOf(dinosaur);
   this.dinosaurs.splice(dinosaurIndex, 1);
+};
+
+Park.prototype.mostPopular = function(){
+  const mostPopularDinosaur = this.dinosaurs.reduce(function(firstDinosaur, secondDinosaur){
+    return (firstDinosaur.guestsAttractedPerDay > secondDinosaur.guestsAttractedPerDay) ? firstDinosaur : secondDinosaur;
+  });
+  return mostPopularDinosaur;
 };
 
 module.exports = Park;
